9f5da65c78475dccc1c869b9ece1f8dcfeca7381
Kemal Sezgen <kemalsezgen@Kemal-MacBook-Pro.local>
2023-03-22 21:11:08 +0300

    login-register refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/LoginPage.js";
+import Register from "./components/RegisterPage.js";
+import Home from "./components/HomePage.js";
+
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
