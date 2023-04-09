0655b042e50598fc68f5280fb6b4f128bc87da56
Kemal Sezgen <kemalsezgen@Kemal-MacBook-Pro.local>
2023-04-10 02:16:01 +0300

    toCamelCase function

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+
+import { toCamelCase } from '../utils/camelCase';
 
 const Profile = () => {
 
   const { currentUser } = useSelector((state) => state.user);
 
   return (
-    <div>
-      Welcome {`${currentUser.username}`}
+    <div className='profile-container'>
+      <div className='profile-header'>
+          <img src={currentUser.photo ? currentUser.photo : 'https://images.unsplash.com/photo-1620371350502-999e9a7d80a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=740&q=80'} alt='trainer' />
+          <h2>{toCamelCase(currentUser.name) + " " + toCamelCase(currentUser.surname)}</h2>
+      </div>
+      <div className='profile-body'>
+
+      </div>
     </div>
   )
 }
